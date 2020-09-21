0a9bf8923458e3bfc309154ca75a76ab76794cfe
BavuyiseD <71077974+BavuyiseD@users.noreply.github.com>
2020-09-21 16:21:44 +0200

    Add files via upload

---
+++ day1_exercise.py
@@ -0,0 +1,25 @@
+#name = str  
+#input(" What is your name " name)
+#print("Hello" + name + "How are you")
+
+name= input(" What is your name ")
+print("Hello " + name + " how are you")
+
+#PI= 3.14
+#radius= int(input( " enter radius "))
+#rea = PI*radius*radius
+#print(area)
+
+radius= int(input("enter radius: "))
+circumference = 2 * 3.14 * radius 
+print( "The circumference is " + str(circumference))
+
+
+
+
+
+
+
+
+
+
